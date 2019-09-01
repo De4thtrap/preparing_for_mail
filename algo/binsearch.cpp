c635c6b2018343e527aadf3efa828efe31cd62ca
FilippovIvan19 <fim1902@gmail.com>
2019-09-01 19:26:21 +0300

    binsearch2

--- algo/binsearch.cpp
+++ algo/binsearch.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int BinarySearch(double *arr, int count, double element)
+int BinarySearch(int *arr, int count, int element)
 {
     int first = 0;
     int last = count - 1;
@@ -15,9 +15,30 @@ int BinarySearch(double *arr, int count, double element)
     return (arr[first] == element)? first : -1;
 }
 
+int* BinarySearch2(const int *start, const int *end, const int element)
+{
+    const int *mid = nullptr;
+    while (start < end)
+    {
+        mid = start + (end - start) / 2;
+        if (element < *mid)
+            end = mid;
+        else if (element > *mid)
+            start = mid + 1;
+        else
+            return (int*)mid;
+    }
+    return (*start == element)? (int*)start : nullptr;
+}
+
 int main(int argc, char const *argv[])
 {
-    double arr[10] = {-6, -1, 2, 3, 4, 5, 7, 8, 9, 15};
-    printf("%d\n", BinarySearch(arr, 2, -6));
+    int arr[10] = {-6, -1, 2, 3, 4, 5, 7, 8, 9, 15};
+    printf("%d\n", BinarySearch(arr, 10, -6));
+    printf("%d\n", BinarySearch(arr, 10, 4));
+    printf("%d\n", BinarySearch(arr, 10, 10));
+    printf("%d\n", *BinarySearch2(arr, arr + 10, -6));
+    printf("%d\n", *BinarySearch2(arr, arr + 10, 4));
+    printf("%d\n", *BinarySearch2(arr, arr + 10, 10));
     return 0;
 }
